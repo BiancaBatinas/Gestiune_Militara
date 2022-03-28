e69256e72e36c86ce3a7a55d0563cf7609f5faeb
Bianca Batinas <biancabatinas64@gmail.com>
2022-03-28 12:58:06 +0300

    Update 0.2

---
+++ GestiuneMilitara/Armament/Arme.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace Armament
+{
+    public class Arme
+    {
+        /*------------Date membre-----------------*/
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+        private const int CATEGORIE = 0;
+        private const int MODEL = 1;
+        private const int TIP = 2;
+        private const int CALIBRU = 3;
+        private const int DETALII = 4;
+        private const int NRARMAMENT = 5;
+
+        string CategorieArmament; //Armament de infanterie
+        string Model; //ZU-2
+        string Tip;//Mitralieră antiaeriană
+        string Calibru;//2×14,5mm
+        string Detalii;//Versiune de producție românească.
+        int NumarArmament;//1000
+        /*------------Sfarsit date membre-----------------*/
+
+        /*------------Inceput memode-----------------*/
+        public Arme()
+        {
+            CategorieArmament = string.Empty;
+            Model = string.Empty;
+            Tip = string.Empty;
+            Calibru = string.Empty;
+            Detalii = string.Empty;
+            NumarArmament = 0;
+        }
+
+        public Arme(string _Categorie, string _Model, string _Tip, string _Calibru, string _Detalii, int _NumarArmament)
+        {
+            CategorieArmament = _Categorie;
+            Model = _Model;
+            Tip = _Tip;
+            Calibru = _Calibru;
+            Detalii = _Detalii;
+            NumarArmament = _NumarArmament;
+        }
+
+        public void Citire_Armament()
+        {
+            CategorieArmament = Console.ReadLine();
+            Model = Console.ReadLine();
+            Tip = Console.ReadLine();
+            Calibru = Console.ReadLine();
+            Detalii = Console.ReadLine();
+            NumarArmament = Console.Read();
+        }
+
+        /*------------Inceput metode GET  -----------------*/
+        public string GetCategorie()
+        {
+            return CategorieArmament;
+        }
+
+        public string GetModel()
+        {
+            return Model;
+        }
+
+        public string GetTip()
+        {
+            return Tip;
+        }
+
+        public string GetCalibru()
+        {
+            return Calibru;
+        }
+
+        public string GetDetalii()
+        {
+            return Detalii;
+        }
+
+        public int GetNumarArmament()
+        {
+            return NumarArmament;
+        }
+        /*------------Sfarsit metode GET  -----------------*/
+
+        /*------------Inceput metode SET-----------------*/
+        public void SetCategorie(string _Categorie)
+        {
+            CategorieArmament = _Categorie;
+        }
+
+        public void SetModel(string _Model)
+        {
+            Model = _Model;
+        }
+
+        public void SetTip(string _Tip)
+        {
+            Tip = _Tip;
+        }
+
+        public void SetCalibru(string _Calibru)
+        {
+            Calibru = _Calibru;
+        }
+
+        public void SetDetalii(string _Detalii)
+        {
+            Detalii = _Detalii;
+        }
+
+        public void SetNumarArmament(int _NumarArmament)
+        {
+            NumarArmament = _NumarArmament;
+        }
+        /*------------Sfarsit metode SET-----------------*/
+        public Arme(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+            CategorieArmament = dateFisier[CATEGORIE];
+            Model = dateFisier[MODEL];
+            Tip = dateFisier[TIP];
+            Calibru = dateFisier[CALIBRU];
+            Detalii = dateFisier[DETALII];
+            NumarArmament = Convert.ToInt32(dateFisier[NRARMAMENT]);
+        }
+
+        public string ConversieLaSir_PentruFisier_armament()
+        {
+            string obiectStudentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}",
+                SEPARATOR_PRINCIPAL_FISIER, (CategorieArmament ?? " NECUNOSCUT "), (Model ?? " NECUNOSCUT "), (Tip ?? " NECUNOSCUT "), (Calibru ?? " NECUNOSCUT "), (Detalii ?? " NECUNOSCUT "), NumarArmament.ToString());
+
+            return obiectStudentPentruFisier;
+        }
+        /*------------Sfarsit memode-----------------*/
+    }
+}
+
--- GestiuneMilitara/GestiuneMilitara/Program.cs
+++ GestiuneMilitara/GestiuneMilitara/Program.cs
@@ -1,15 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using Armament;
+using Soldat;
+using Administrare;
 
-namespace GestiuneMilitara
+namespace AplicatieDeGestiune
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            string _numeFisier = ConfigurationManager.AppSettings["_NumeFisier"];
+            Administrare_informatii AdministrareSoldati = new Administrare_informatii(numeFisier);
+            Administrare_informatii AdministrareArmament = new Administrare_informatii(_numeFisier);
+            int nrSoldati = 0;
+            int nrArmament = 0;
+            AdministrareSoldati.GetSoldati(out nrSoldati);
+            AdministrareArmament.Get_Armament(out nrArmament);
+
+
+            Soldati soldat = new Soldati();
+            Arme Arme = new Arme();
+            string optiune;
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("------------------------F. Afisare soldati din fisier------------------------");
+                Console.WriteLine("------------------------S. Inregistrare soldat       ------------------------");
+                Console.WriteLine("------------------------A. Afisare armament          ------------------------");
+                Console.WriteLine("------------------------H. Inregistrare arma         ------------------------");
+                Console.WriteLine("------------------------C. Citire nume soldat        ------------------------");
+                Console.WriteLine("------------------------K. Citire armament           ------------------------");
+                Console.WriteLine("------------------------L. Cautare soldat            ------------------------");
+                Console.WriteLine("------------------------X. Inchidere program         ------------------------");
+                Console.WriteLine("------------------------Alegeti o optiune            ------------------------\n");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "F":
+                        Soldati[] soldati = AdministrareSoldati.GetSoldati(out nrSoldati);
+                        AfisareSoldati(soldati, nrSoldati);
+                        break;
+                    case "A":
+                        Arme[] arme = AdministrareArmament.Get_Armament(out nrArmament);
+                        AfisareArmament(arme, nrArmament);
+                        break;
+                    case "S":
+                        nrSoldati = nrSoldati + 1;
+                        AdministrareSoldati.AddSoldat(soldat);
+                        break;
+                    case "H":
+                        nrArmament = nrArmament + 1;
+                        AdministrareArmament.AddArmament(Arme);
+                        break;
+                    case "C":
+                        soldat.Citire_Soldat();
+                        Console.WriteLine(soldat.InformatiiSoldat());
+                        break;
+                    case "K":
+                        Arme.Citire_Armament();
+                        break;
+                    case "L":
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+
+
+        }
+        public static void AfisareSoldati(Soldati[] soldati, int nrSoldati)
+        {
+            Console.WriteLine("Soldatii sunt:");
+            for (int contor = 0; contor < nrSoldati; contor++)
+            {
+                string infoStudent = string.Format("Soldatul cu numele {0} {1} este nascut la data de  {2} si se afla la unitatea {3}",
+                   (soldati[contor].GetNumeSoldat() ?? " NECUNOSCUT "),
+                   (soldati[contor].GetPrenumeSoldat() ?? " NECUNOSCUT "),
+                   (soldati[contor].GetDataNasterii() ?? " NECUNOSCUT "),
+                   (soldati[contor].GetUnitate() ?? " NECUNOSCUT "));
+
+                Console.WriteLine(infoStudent);
+            }
+        }
+
+        public static void AfisareArmament(Arme[] arme, int nrArme)
+        {
+            Console.WriteLine("Echipamentele sunt:");
+            for (int contor = 0; contor < nrArme; contor++)
+            {
+                string infoArme = string.Format("Categorie: {0} \n Model: {1} \n Tip: {2} \n Calibru: {3} \n Detalii arma: {4} \n Cantitate: {5}",
+                   (arme[contor].GetCategorie() ?? " NECUNOSCUT "),
+                   (arme[contor].GetModel() ?? " NECUNOSCUT "),
+                   (arme[contor].GetTip() ?? " NECUNOSCUT "),
+                   (arme[contor].GetCalibru() ?? " NECUNOSCUT "),
+                   (arme[contor].GetDetalii() ?? " NECUNOSCUT "),
+                   arme[contor].GetNumarArmament());
+
+                Console.WriteLine(infoArme);
+            }
         }
     }
 }
